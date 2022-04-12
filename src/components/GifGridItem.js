c99c92007bc179ff3d6e67d51199cf62b40547ee
Wilmer León Chávez <wleonch@hotmail.com>
2022-04-12 00:02:02 -0500

    gif grid item component's test

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-const GifGridItem = ({title,url}) => {
+const GifGridItem = ({ title, url }) => {
   return (
     <figure className='card animate__animated animate__fadeIn'>
-        <img className='card__image' src={url} alt={title}/>
-        <figcaption className='card__caption'>{title}</figcaption>
+      <img className='card__image' src={url} alt={title} />
+      <figcaption className='card__caption'>{title}</figcaption>
     </figure>
   )
+}
+
+GifGridItem.protoTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 }
 
 export default GifGridItem;
--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -4,8 +4,28 @@ import { shallow } from 'enzyme';
 import GifGridItem from '../../components/GifGridItem';
 
 describe("Tests in <GifGridItem />", () => {
-    test('Debe de mostrar el componente correctamente', () => { 
-        const wrapper = shallow(<GifGridItem />)
-        expect( wrapper ).toMatchSnapshot();
-     })
+
+    const title = "Un titulo";
+    const url = "https://algo.com"
+    const wrapper = shallow(<GifGridItem title={title} url={url} />)
+
+    test('Debe de mostrar el componente correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Debe tener un párrafo con el prop.: title', () => {
+        const p = wrapper.find('figcaption');
+        expect(p.text().trim()).toBe(title);
+    });
+
+    test('Debe tener una imagen con las props.: \'title\' y \'url\'', () => {
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url); // img.props().src
+        expect(img.prop('alt')).toBe(title); // img.props().alt
+    });
+
+    test('Debe tener la clase: animate__fadeIn', () => {
+        const figure = wrapper.find('figure');
+        expect(figure.hasClass('animate__fadeIn')).toBe(true)
+    })
 })
